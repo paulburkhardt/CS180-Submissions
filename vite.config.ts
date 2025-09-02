033f71dbe4ef34c6164518ea2c711899c4fec44f
paulburkhardt <92929336+paulburkhardt@users.noreply.github.com>
2025-09-01 20:37:28 -0700

    host-on-gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig(({ mode }: { mode: string }) => ({
+  base: mode === 'production' ? '/please-let-me-in-CS180/' : '/',
   server: {
     host: "::",
     port: 8080,
@@ -12,7 +12,6 @@ export default defineConfig(({ mode }) => ({
   plugins: [
     react(),
     mode === 'development' &&
-    componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
